6bd7ac1997f2a657af453c6e761cef66d38d2c11
ValeriiaMalyshko <91088527+ValeriiaMalyshko@users.noreply.github.com>
2022-02-27 12:14:01 +0200

    task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,22 @@
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
 
+let timerId = null;
+startBtn.disabled = false;
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
+
+startBtn.addEventListener("click", () => {
+  timerId = setInterval(() => {
+  let color = getRandomHexColor() ;
+  document.body.style.backgroundColor = `${color}`;
+  }, 1000);
+  startBtn.disabled = true;
+});
+
+
+stopBtn.addEventListener("click", () => {
+  clearInterval(timerId);
+  startBtn.disabled = false;
+});
