eba23e179c181950c320c3564493539ab14dce16
ValeriiaMalyshko <91088527+ValeriiaMalyshko@users.noreply.github.com>
2022-03-01 18:48:46 +0200

    task 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,41 @@
+import Notiflix from 'notiflix';
+
+const formBlock = document.querySelector(".form");
+
+formBlock.addEventListener("submit", (event) => {
+  
+  event.preventDefault();
+  const { delay, step, amount }  = event.currentTarget;
+
+  let currentDelay = Number(delay.value);
+  const stepNumber = Number(step.value);
+  const amountP = Number(amount.value);
+
+  for (let position = 1; position <= amountP; position++) {
+    createPromise(position, currentDelay);
+    console.log("delayPromise", currentDelay, "position", position);
+    currentDelay += stepNumber;
+  };
+});
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
-}
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      };
+    }, delay);
+  });
+
+  promise
+  .then(({ position, delay }) => {
+      Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+  })
+  .catch(({ position, delay }) => {
+      Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+    });
+};
+
